2aa4ee24d46dc802271d1260e89291e04be4173c
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:51 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onUninstall.php
+++ include/onUninstall.php
@@ -17,6 +17,8 @@
  * @version    $Id $
  **/
 
+use XoopsModules\Tad_timeline\Utility;
+
 function xoops_module_uninstall_tad_timeline($module)
 {
     global $xoopsDB;
@@ -25,54 +27,4 @@ function xoops_module_uninstall_tad_timeline($module)
     rename(XOOPS_ROOT_PATH . "/uploads/tad_timeline", XOOPS_ROOT_PATH . "/uploads/tad_timeline_bak_{$date}");
 
     return true;
-}
-
-//刪除目錄
-function tad_timeline_delete_directory($dirname)
-{
-    if (is_dir($dirname)) {
-        $dir_handle = opendir($dirname);
-    }
-
-    if (!$dir_handle) {
-        return false;
-    }
-
-    while ($file = readdir($dir_handle)) {
-        if ($file != "." && $file != "..") {
-            if (!is_dir($dirname . "/" . $file)) {
-                unlink($dirname . "/" . $file);
-            } else {
-                tad_timeline_delete_directory($dirname . '/' . $file);
-            }
-
-        }
-    }
-    closedir($dir_handle);
-    rmdir($dirname);
-    return true;
-}
-
-//拷貝目錄
-function tad_timeline_full_copy($source = "", $target = "")
-{
-    if (is_dir($source)) {
-        @mkdir($target);
-        $d = dir($source);
-        while (false !== ($entry = $d->read())) {
-            if ($entry == '.' || $entry == '..') {
-                continue;
-            }
-
-            $Entry = $source . '/' . $entry;
-            if (is_dir($Entry)) {
-                tad_timeline_full_copy($Entry, $target . '/' . $entry);
-                continue;
-            }
-            copy($Entry, $target . '/' . $entry);
-        }
-        $d->close();
-    } else {
-        copy($source, $target);
-    }
 }
